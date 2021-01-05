1089ca487f8b373e8ba5188a46700061f96c2a24
Alex-Lin-Fei <2055017970@qq.com>
2021-01-05 19:13:54 +0800

    add

--- 830/main.cpp
+++ 830/main.cpp
@@ -8,18 +8,27 @@ vector<vector<int>> largeGroupPositions(string s) {
     int start = 0, end = 0;
     while (start < s.size()) {
 //       二分法找到end
-        int l = start, r = s.size() - 1;
-        while (l < r) {
+        int l = start + 1, r = s.size() - 1;
+        while (l <= r) {
+            int m = (l + r) >> 1;
+            if (s[m] == s[start])
+                l = m + 1;
+            else
+                r = m - 1;
         }
-
-
+        end = l - 1;
         ans.push_back({start, end});
         start = end + 1;
     }
+
+    for (auto &item: ans)
+        cout << item[0] << ' ' << item[1] << endl;
+
     return ans;
 }
 
 int main() {
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
