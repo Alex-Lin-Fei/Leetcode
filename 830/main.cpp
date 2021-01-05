29b46d6e6096cfc6d3ccb3cbc3a24f709a773dab
Alex-Lin-Fei <2055017970@qq.com>
2021-01-05 18:03:59 +0800

    edit a part of function

--- 830/main.cpp
+++ 830/main.cpp
@@ -1,4 +1,19 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+
+vector<vector<int>> largeGroupPositions(string s) {
+    vector<vector<int>> ans;
+    int start = 0, end = 0;
+    while (start < s.size()) {
+//       二分法找到end
+
+        ans.push_back({start, end});
+        start = end + 1;
+    }
+    return ans;
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
