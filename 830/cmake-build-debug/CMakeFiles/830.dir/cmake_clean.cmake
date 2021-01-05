43f8cf5c1666842e0eb33e3d5c947d7c29e8b3a2
Alex-Lin-Fei <2055017970@qq.com>
2021-01-05 17:55:12 +0800

    add 830

---
+++ 830/cmake-build-debug/CMakeFiles/830.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "830.exe"
+  "830.exe.manifest"
+  "830.pdb"
+  "CMakeFiles/830.dir/main.cpp.obj"
+  "lib830.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/830.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 830/cmake-build-debug/CMakeFiles/830.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for 830.
+# This may be replaced when dependencies are built.
