6b139d38c796306a9016eaa1d953754483e8d5e4
Alex-Lin-Fei <2055017970@qq.com>
2021-01-10 13:40:55 +0800

    end edit

--- 228/main.cpp
+++ 228/main.cpp
@@ -3,10 +3,37 @@
 using namespace std;
 
 vector<string> summaryRanges(vector<int>& nums) {
+    vector<string> ans;
 
+    if (nums.empty())
+        return ans;
+    int start = 0;
+    int end =0;
+
+    for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] == nums[i - 1] + 1)
+            continue;
+        else {
+            end = i - 1;
+            if (end > start)
+                ans.push_back(to_string(nums[start]) + "->" + to_string(nums[end]));
+            else
+                ans.push_back(to_string(nums[start]));
+            start = i;
+        }
+    }
+    if (start == nums.size()-1)
+        ans.push_back(to_string(nums[start]));
+    else
+        ans.push_back(to_string(nums[start]) +"->" + to_string(nums.back()));
+
+    for (auto &str: ans)
+        cout << str << endl;
+    return ans;
 }
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> nums{0,2,3,4,6,8,9};
+    summaryRanges(nums);
     return 0;
 }
