d7d507661971f1bf262845e4b852caa7891fd6da
Alex-Lin-Fei <2055017970@qq.com>
2021-01-10 12:57:42 +0800

    start edit

--- 228/main.cpp
+++ 228/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+vector<string> summaryRanges(vector<int>& nums) {
+
+}
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
