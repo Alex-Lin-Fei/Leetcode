5792c867d7b5a2b413c9312a3b4366a67e17a107
Alex-Lin-Fei <2055017970@qq.com>
2021-01-11 14:03:06 +0800

    add 1202

---
+++ 1202/cmake-build-debug/CMakeFiles/1202.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1202.exe"
+  "1202.exe.manifest"
+  "1202.pdb"
+  "CMakeFiles/1202.dir/main.cpp.obj"
+  "lib1202.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1202.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1202/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+string smallestStringWithSwaps(string s, vector<vector<int>>& pairs) {
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
