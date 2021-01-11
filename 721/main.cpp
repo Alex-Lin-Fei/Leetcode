2b96090ee72817d142d0dc1e03ffba75dc65ade3
Alex-Lin-Fei <2055017970@qq.com>
2021-01-11 19:10:21 +0800

    unite find set

--- 721/main.cpp
+++ 721/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <set>
 using namespace std;
 
 class UnionFind{
@@ -41,7 +43,48 @@ public:
 };
 
 
+vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
+    UnionFind unionFind = UnionFind(accounts.size());
+    map<int, set<string>> hash;
+    map<string, int> father;
+
+//    加边
+   for (int i = 0; i < accounts.size(); i++)
+       for (int j = 1; j < accounts[i].size(); j++) {
+           if (!father.count(accounts[i][j]))
+               father[accounts[i][j]] = i;
+
+           else
+               unionFind.unite(i, father[accounts[i][j]]);
+
+       }
+
+    for (int i = 0; i < accounts.size(); i++) {
+        for (int j = 1; j < accounts[i].size(); j++)
+            hash[unionFind.findFather(i)].insert(accounts[i][j]);
+    }
+
+    vector<vector<string>> ans;
+    for (auto &item: hash) {
+        vector<string> account;
+//        账户名字
+        account.push_back(accounts[item.first][0]);
+//        加入邮箱
+        for (auto &email: item.second)
+            account.push_back(email);
+        ans.push_back(account);
+    }
+
+    return ans;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<vector<string>> accounts = {{"Alex","Alex5@m.co","Alex4@m.co","Alex0@m.co"},{"Ethan","Ethan3@m.co","Ethan3@m.co","Ethan0@m.co"},
+                                       {"Kevin","Kevin4@m.co","Kevin2@m.co","Kevin2@m.co"},{"Gabe","Gabe0@m.co","Gabe3@m.co","Gabe2@m.co"},
+                                       {"Gabe","Gabe3@m.co","Gabe4@m.co","Gabe2@m.co"}};
+
+accountsMerge(accounts);
+
+
     return 0;
 }
