dc1028c5b38897fe77b20daaf01832362a1fe871
Alex-Lin-Fei <2055017970@qq.com>
2021-01-11 16:05:53 +0800

    add 947 union find set

---
+++ 947/cmake-build-debug/CMakeFiles/947.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "947.exe"
+  "947.exe.manifest"
+  "947.pdb"
+  "CMakeFiles/947.dir/main.cpp.obj"
+  "lib947.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/947.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
