18ea177bc0f30f5314a0881b439e7a02d3d5bab9
Alex-Lin-Fei <2055017970@qq.com>
2021-01-11 14:34:19 +0800

    add 1631

---
+++ 1631/cmake-build-debug/CMakeFiles/1631.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1631.exe"
+  "1631.exe.manifest"
+  "1631.pdb"
+  "CMakeFiles/1631.dir/main.cpp.obj"
+  "lib1631.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1631.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
