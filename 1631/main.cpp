989d8f08bea2267a0076c4f91cff46daa428f7e4
Alex-Lin-Fei <2055017970@qq.com>
2021-01-11 14:50:59 +0800

    accomplish(union find set)

--- 1631/main.cpp
+++ 1631/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <tuple>
+#include <algorithm>
 #include <vector>
 using namespace std;
 
@@ -41,6 +43,32 @@ public:
 };
 
 int minimumEffortPath(vector<vector<int>>& heights) {
+    int m = heights.size(), n = heights[0].size();
+    int size = m * n;
+    UnionFind unionFind = UnionFind(size);
+
+//    为边排序  每个位置的边为右方向和下方的点以及绝对值组成
+    vector<tuple<int, int, int>> edges;
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++) {
+//            往下的边
+            if (i != m - 1)
+                edges.emplace_back(i * n + j, i * n + j + n, abs(heights[i][j] - heights[i + 1][j]));
+//            往右的边
+            if (j != n - 1)
+                edges.emplace_back(i * n + j, i * n + j + 1, abs(heights[i][j] - heights[i][j + 1]));
+        }
+    sort(edges.begin(), edges.end(), [](tuple<int, int, int> a, tuple<int, int, int> b) {
+        return get<2>(a) < get<2>(b);
+    });
+
+//        加边
+    for (auto &edge: edges) {
+        if (!unionFind.isSame(get<0>(edge), get<1>(edge)))
+            unionFind.unite(get<0>(edge), get<1>(edge));
+        if (unionFind.isSame(0, m * n - 1))
+            return get<2>(edge);
+    }
 
 }
 
