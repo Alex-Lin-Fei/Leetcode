1d29ae04e703ad0a7eae12c7501b71fb12489369
Alex-Lin-Fei <2055017970@qq.com>
2021-01-14 10:10:54 +0800

    three times reverse

--- 189/main.cpp
+++ 189/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void rotate(vector<int>& nums, int k) {
+    int start = (nums.size() - k % nums.size()) % nums.size();
+//    反转两部分
+    reverse(nums.begin(), nums.begin() + start);
+    reverse(nums.begin() + start, nums.end());
+    reverse(nums.begin(), nums.end());
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> nums{1,3,2,4,7,5};
+    rotate(nums, 3);
+
     return 0;
 }
