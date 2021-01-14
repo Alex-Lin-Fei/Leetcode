70d0e1ed752b49be3a6aa9ad062e304de0516814
Alex-Lin-Fei <2055017970@qq.com>
2021-01-14 09:33:35 +0800

    add 1018

---
+++ 778/cmake-build-debug/CMakeFiles/778.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "778.exe"
+  "778.exe.manifest"
+  "778.pdb"
+  "CMakeFiles/778.dir/main.cpp.obj"
+  "lib778.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/778.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
