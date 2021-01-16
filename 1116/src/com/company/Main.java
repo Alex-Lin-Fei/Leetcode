9900c55fde58548276333cdb1596295b5893e076
Alex-Lin-Fei <2055017970@qq.com>
2021-01-16 14:38:19 +0800

    add 1116 in Java

---
+++ 1116/src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class ZeroEvenOdd {
+    private int n;
+
+    public ZeroEvenOdd(int n) {
+        this.n = n;
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void zero(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void even(IntConsumer printNumber) throws InterruptedException {
+
+    }
+
+    public void odd(IntConsumer printNumber) throws InterruptedException {
+
+    }
+}
