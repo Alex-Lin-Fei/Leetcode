cd26025f31d0f0b1422bab516498a7f598708425
Alex-Lin-Fei <2055017970@qq.com>
2021-01-16 15:38:24 +0800

    add 1195 in Java

---
+++ 1195/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.util.function.IntConsumer;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+
+class FizzBuzz {
+    private int n;
+
+    public FizzBuzz(int n) {
+        this.n = n;
+    }
+
+    // printFizz.run() outputs "fizz".
+    public void fizz(Runnable printFizz) throws InterruptedException {
+
+    }
+
+    // printBuzz.run() outputs "buzz".
+    public void buzz(Runnable printBuzz) throws InterruptedException {
+
+    }
+
+    // printFizzBuzz.run() outputs "fizzbuzz".
+    public void fizzbuzz(Runnable printFizzBuzz) throws InterruptedException {
+
+    }
+
+    // printNumber.accept(x) outputs "x", where x is an integer.
+    public void number(IntConsumer printNumber) throws InterruptedException {
+
+    }
+}
