f7592db381f6c2390312d98c13660fe76f7be864
Alex-Lin-Fei <2055017970@qq.com>
2021-01-17 00:02:40 +0800

    add 01.08 in C++

---
+++ 01.08/cmake-build-debug/CMakeFiles/01_08.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "01_08.exe"
+  "01_08.exe.manifest"
+  "01_08.pdb"
+  "CMakeFiles/01_08.dir/main.cpp.obj"
+  "lib01_08.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/01_08.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01.08/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void setZeroes(vector<vector<int>>& matrix) {
+    set<int> rows, cols;
+
+    for (int i = 0; i < matrix.size(); i++)
+        for (int j = 0; j < matrix[0].size(); j++)
+            if (!matrix[i][j])
+            {
+                rows.insert(i);
+                cols.insert(j);
+            }
+
+    for (auto & row: rows) {
+        for (int j = 0; j < matrix[row].size(); j++)
+            matrix[row][j] = 0;
+    }
+
+    for (auto & col: cols) {
+        for (int i = 0; i < matrix.size(); i++)
+            matrix[i][col] = 0;
+    }
+}
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
