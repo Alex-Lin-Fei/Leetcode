7ab8295d8e70074dd5bbe37c6a9a32131166277e
Alex-Lin-Fei <2055017970@qq.com>
2021-01-18 09:53:26 +0800

    map

--- 16.02/main.cpp
+++ 16.02/main.cpp
@@ -1,4 +1,29 @@
 #include <iostream>
+#include <map>
+#include <vector>
+using namespace std;
+
+
+class WordsFrequency {
+private:
+    map<string ,int> count;
+public:
+    explicit WordsFrequency(vector<string>& book) {
+        for (auto& b: book)
+            count[b]++;
+    }
+
+    int get(const string& word) {
+        return count[word];
+    }
+};
+
+/**
+ * Your WordsFrequency object will be instantiated and called as such:
+ * WordsFrequency* obj = new WordsFrequency(book);
+ * int param_1 = obj->get(word);
+ */
+
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
