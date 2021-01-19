c56d257fd9b286a9855de9fbf7701d08a377f1d4
Alex-Lin-Fei <2055017970@qq.com>
2021-01-19 14:32:11 +0800

    stack

--- Offer31/main.cpp
+++ Offer31/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+    stack<int> s;
+    int idx = 0;
+
+    for (int i : popped) {
+        while (s.empty() || s.top() != i) {
+            if (idx == pushed.size())
+                return false;
+            s.push(pushed[idx++]);
+        }
+        s.pop();
+    }
+
+    return s.empty();
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> pushed{1,2,3,4,5};
+    vector<int> poped{4,5,3,2,1};
+    std::cout << validateStackSequences(pushed, poped) << std::endl;
+
     return 0;
 }
