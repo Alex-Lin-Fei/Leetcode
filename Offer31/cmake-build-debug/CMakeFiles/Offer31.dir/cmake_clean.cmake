7d2d5813b2b62321cce386dffef92b999dca85b4
Alex-Lin-Fei <2055017970@qq.com>
2021-01-19 14:26:21 +0800

    add Offer 31

---
+++ Offer31/cmake-build-debug/CMakeFiles/Offer31.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Offer31.dir/main.cpp.obj"
+  "Offer31.exe"
+  "Offer31.exe.manifest"
+  "Offer31.pdb"
+  "libOffer31.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Offer31.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
