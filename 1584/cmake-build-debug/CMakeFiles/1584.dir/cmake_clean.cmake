75ab9cbcc4bb4cd098b8bb0d7fa6cfc5a923bd01
Alex-Lin-Fei <2055017970@qq.com>
2021-01-19 10:56:55 +0800

    add 1584

---
+++ 1584/cmake-build-debug/CMakeFiles/1584.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1584.exe"
+  "1584.exe.manifest"
+  "1584.pdb"
+  "CMakeFiles/1584.dir/main.cpp.obj"
+  "lib1584.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1584.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
