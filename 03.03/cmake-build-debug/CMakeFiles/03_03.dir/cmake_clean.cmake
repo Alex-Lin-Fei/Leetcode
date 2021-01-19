dad52fd3e516884788527593df4ae8e3c2b722fe
Alex-Lin-Fei <2055017970@qq.com>
2021-01-19 12:04:58 +0800

    add 03.03

---
+++ 03.03/cmake-build-debug/CMakeFiles/03_03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "03_03.exe"
+  "03_03.exe.manifest"
+  "03_03.pdb"
+  "CMakeFiles/03_03.dir/main.cpp.obj"
+  "lib03_03.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/03_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
