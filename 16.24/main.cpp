2e53744ac5888ffd8b230c1c63812b2dea4903a9
Alex-Lin-Fei <2055017970@qq.com>
2021-01-20 22:28:10 +0800

    binary search (over time) two pointer l and r

--- 16.24/main.cpp
+++ 16.24/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> pairSums(vector<int>& nums, int target) {
+    vector<vector<int>> ans;
+
+    if (nums.empty())
+        return ans;
+    sort(nums.begin(), nums.end());
+
+
+////    二分法
+//    for (int i = 0; i < nums.size() - 1; i++) {
+//        if (nums[i] >= target)
+//            break;
+//        auto iter = find(nums.begin() + i + 1, nums.end(), target - nums[i]);
+//        if (iter != nums.end()) {
+//            ans.push_back({nums[i], *iter});
+//            nums.erase(iter);
+//        }
+//    }
+
+    int l = 0, r = nums.size() - 1;
+    while (l < r) {
+        if (nums[l] + nums[r] > target)
+            r--;
+        else if (nums[l] + nums[r] < target)
+            l++;
+        else
+            ans.push_back({nums[l++], nums[r--]});
+    }
+
+    return ans;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> nums{};
+    auto ans = pairSums(nums, 0);
+    for (auto &i: ans)
+        cout << i[0] << ' ' << i[1] << endl;
+
     return 0;
 }
