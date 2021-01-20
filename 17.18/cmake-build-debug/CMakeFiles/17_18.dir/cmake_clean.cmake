b1d8c55dd5157253e93c9454ed2af699c8310802
Alex-Lin-Fei <2055017970@qq.com>
2021-01-20 20:17:45 +0800

    add 17.18

---
+++ 17.18/cmake-build-debug/CMakeFiles/17_18.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "17_18.exe"
+  "17_18.exe.manifest"
+  "17_18.pdb"
+  "CMakeFiles/17_18.dir/main.cpp.obj"
+  "lib17_18.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/17_18.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 17.18/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> shortestSeq(vector<int>& big, vector<int>& small) {
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
