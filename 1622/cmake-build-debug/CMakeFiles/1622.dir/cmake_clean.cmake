268b415aa05794f9afa63e5a9dea7fbb65e30a5a
Alex-Lin-Fei <2055017970@qq.com>
2021-01-25 23:23:41 +0800

    add 1622 (empty)

---
+++ 1622/cmake-build-debug/CMakeFiles/1622.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1622.exe"
+  "1622.exe.manifest"
+  "1622.pdb"
+  "CMakeFiles/1622.dir/main.cpp.obj"
+  "lib1622.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1622.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1622/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    std::cout << 3.14*2/270+sqrt(3)/3 << std::endl;
+    std::cout << 1.01*exp(1) << std::endl;
+    cout << 0.5 - 3.14 / 180 * sqrt(3) / 2 <<endl;
+
+    return 0;
+}
