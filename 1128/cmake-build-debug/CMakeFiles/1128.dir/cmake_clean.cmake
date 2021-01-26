ac0725089b9438c2d0e0627c6939ba30f2b4f11c
Alex-Lin-Fei <2055017970@qq.com>
2021-01-26 09:17:28 +0800

    add 1128

---
+++ 1128/cmake-build-debug/CMakeFiles/1128.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1128.exe"
+  "1128.exe.manifest"
+  "1128.pdb"
+  "CMakeFiles/1128.dir/main.cpp.obj"
+  "lib1128.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1128.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1128/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
