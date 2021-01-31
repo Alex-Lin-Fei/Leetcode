9ec5e0ba57728de5fd7c14bdd294b582ef5dce81
Alex-Lin-Fei <2055017970@qq.com>
2021-01-31 09:56:40 +0800

    stack push(-1)

--- 32/main.cpp
+++ 32/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+int longestValidParentheses(string s) {
+    int len = 0;
+    stack<int> stk;
+    stk.push(-1);
+
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '(')
+            stk.push(i);
+        else {
+            stk.pop();
+            if (stk.empty())
+                stk.push(i);
+            else {
+                len = max(len, i-stk.top());
+            }
+        }
+    }
+    return len;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << longestValidParentheses(")()())") << std::endl;
     return 0;
 }
