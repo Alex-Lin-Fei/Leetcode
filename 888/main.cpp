3cc18a6058673b61ce5f70744a7600bb6b3c021f
Alex-Lin-Fei <2055017970@qq.com>
2021-02-02 20:18:28 +0800

    set

--- 888/main.cpp
+++ 888/main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+#include <vector>
+#include <numeric>
+#include <set>
+#include <cassert>
+
+using namespace std;
+
+vector<int> fairCandySwap(vector<int>& A, vector<int>& B) {
+    int delta = accumulate(A.begin(), A.end(), 0) - accumulate(B.begin(), B.end(), 0);
+    delta /= 2;
+    set<int> sA;
+
+    for (auto &item: A)
+        sA.insert(item - delta);
+    for (auto &item : B)
+        if (sA.count(item))
+            return {item + delta, item};
+
+    return {};
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> A{1,2,5}, B{2,4};
+    vector<int> ans = fairCandySwap(A,B);
+    std::cout << ans[0] << ans[1] << std::endl;
     return 0;
 }
