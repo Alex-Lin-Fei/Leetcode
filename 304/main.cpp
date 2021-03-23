c366264cd1bbc01de649963be1e27aa699437a4a
Alex-Lin-Fei <2055017970@qq.com>
2021-03-23 09:50:00 +0800

    ?: priority

--- 304/main.cpp
+++ 304/main.cpp
@@ -3,13 +3,27 @@
 using namespace std;
 
 class NumMatrix {
+private:
+    vector<vector<int>> row_sum;
 public:
     explicit NumMatrix(vector<vector<int>>& matrix) {
+        row_sum.resize(matrix.size(), vector<int>(matrix[0].size()));
 
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                row_sum[i][j] = j == 0 ? matrix[i][j] : row_sum[i][j - 1] + matrix[i][j];
+            }
+        }
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            cout << row_sum[i][col2] << '-' << (col1 == 0 ? 0 : row_sum[i][col1 - 1]) << " = ";
+            cout << row_sum[i][col2] - (col1 == 0 ? 0 : row_sum[i][col1 - 1]) << ' ';
+            cout << (row_sum[i][col2] - col1 == 0 ? 0 : row_sum[i][col1 - 1]) << endl;
+        }
+        return sum;
     }
 };
 
@@ -20,6 +34,15 @@ public:
  */
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<vector<int>> matrix{
+            {3, 0, 1, 4, 2},
+            {5, 6, 3, 2, 1},
+            {1, 2, 0, 1, 5},
+            {4, 1, 0, 1, 7},
+            {1, 0, 3, 0, 5}
+    };
+    NumMatrix numMatrix = NumMatrix(matrix);
+
+    std::cout << numMatrix.sumRegion(2,1,4,3) << std::endl;
     return 0;
 }
