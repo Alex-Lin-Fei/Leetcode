a79cdf8edb0a784acd8c662836af0d30f7713266
Alex-Lin-Fei <2055017970@qq.com>
2021-04-06 22:09:07 +0800

    init

---
+++ 1520/cmake-build-debug/CMakeFiles/1520.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "1520.exe"
+  "1520.exe.manifest"
+  "1520.pdb"
+  "CMakeFiles/1520.dir/main.cpp.obj"
+  "lib1520.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1520.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 1520/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<string> maxNumOfSubstrings(string s) {
+//    每个字符初始位置确定的区间
+
+
+//    得到最多区间数且区间长度和最小
+
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
