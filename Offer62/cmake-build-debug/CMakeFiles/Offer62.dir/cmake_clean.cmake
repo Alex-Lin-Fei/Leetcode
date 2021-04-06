e9c41bc725a128162a430dc623335bd0b480e8fa
Alex-Lin-Fei <2055017970@qq.com>
2021-04-06 12:04:26 +0800

    joseph

---
+++ Offer62/cmake-build-debug/CMakeFiles/Offer62.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Offer62.dir/main.cpp.obj"
+  "Offer62.exe"
+  "Offer62.exe.manifest"
+  "Offer62.pdb"
+  "libOffer62.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Offer62.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Offer62/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//over time
+//int lastRemaining(int n, int m) {
+//    vector<int> arr;
+//    auto idx = 0;
+//
+//    arr.reserve(n);
+//for (int i = 0; i < n; i++)
+//        arr.push_back(i);
+//
+//    while (arr.size() > 1) {
+//        idx = (idx + m - 1) % arr.size();
+//        arr.erase(arr.begin() + idx);
+//    }
+//
+//    return arr[0];
+//}
+
+int f(int n, int m) {
+
+}
+
+int lastRemaining(int n, int m) {
+    if (n == 0)
+        return 0;
+    else
+        return (lastRemaining(n - 1, m) + m) % n;
+}
+
+int main() {
+    std::cout << lastRemaining(5,3) << std::endl;
+    return 0;
+}
