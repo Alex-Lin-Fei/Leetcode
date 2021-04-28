1bc99b76ed6a9bad02148492db70148d76791962
Alex-Lin-Fei <2055017970@qq.com>
2021-04-28 14:31:07 +0800

    dp

--- 91/main.cpp
+++ 91/main.cpp
@@ -9,24 +9,26 @@ int numDecodings(string s) {
 
     dp[0] = 1;
     for (int i = 1; i <= s.length(); i++) {
-        if (s[i-1] == '0')
-            dp[i] = dp[i-2];
+        if (s[i-1] == '0') {
+            if (i >= 2 && (s[i - 2] == '1' || s[i - 2] == '2'))
+                dp[i] = dp[i - 2];
+            else
+                return 0;
+        }
         else {
-            if (i == 1)
+            if (i == 1 || s[i-2] > '2' || s[i-2] == '0' || (s[i-2] == '2' && s[i-1] > '6'))
                 dp[i] = dp[i-1];
             else {
-                
+                dp[i] = dp[i-1] + dp[i-2];
             }
         }
-
     }
-
 
     return dp.back();
 }
 
 int main() {
 
-    std::cout << numDecodings("123") << std::endl;
+    std::cout << numDecodings("60") << std::endl;
     return 0;
 }
